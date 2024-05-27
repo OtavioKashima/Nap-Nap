571e2881d75f0d6808851744382b6f229a39c7b8
SmiguelRbr <100735645+SmiguelRbr@users.noreply.github.com>
2024-05-27 13:44:34 -0300

    Add files via upload

---
+++ menuzinho/Miguel Rubens/tablateral.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('collapsed');
+}
+
+document.getElementById('sidebar-toggle').addEventListener('click', toggleSidebar);
